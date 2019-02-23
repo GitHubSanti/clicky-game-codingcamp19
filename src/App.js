2bb02bb1e3a7abadde4f8e113cbe00a496910a40
Spiderman <santiagosepulveda@me.com>
2019-02-23 16:48:41 -0500

    updated clicky game

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Nav from "./components/Navbar";
+// import Game from "./components/Game";
+// import Footer from "./components/Footer";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+        {/* <Game /> */}
+        {/* <Footer /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
